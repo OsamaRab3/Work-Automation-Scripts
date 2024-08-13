566014d4edd0e8ce5c204269216bc0ecab4c154d
osama <osrab3@gmail.com>
2024-08-13 19:43:11 +0300

    screenshot script

---
+++ screenshot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the directory to save screenshots
+SAVE_DIR="$HOME/Pictures/screenshots"
+mkdir -p "$SAVE_DIR"
+
+# Define the filename with a timestamp
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+FILENAME="screenshot_$TIMESTAMP.png"
+
+# Take a screenshot
+gnome-screenshot -f "$SAVE_DIR/$FILENAME"
+
+
+echo "Screenshot saved to $SAVE_DIR/$FILENAME"
