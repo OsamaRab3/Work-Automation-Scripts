4da6a5c08f44b55cef8f14ed472c7e405ec375c5
osama <osrab3@gmail.com>
2024-08-16 17:48:09 +0300

    executes the ps command to display the list of processes running on the system.

---
+++ Process.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#  the script executes the ps command to display the list of processes running on the system.
+
+echo "Hello $USER"
+echo "Hey i am $USER and will be telling you about the current processes"
+echo "Running processes List"
+ps
